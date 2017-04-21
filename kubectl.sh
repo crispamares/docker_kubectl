1b7fefb89de687d0b656b68738c22688e777462a
Juan Morales <crispamares@gmail.com>
2017-04-21 18:54:54 +0200

    scape commas in sed

--- kubectl.sh
+++ kubectl.sh
@@ -3,7 +3,7 @@
 FILE=`echo -n "${@:1}" | gawk 'match($0, /-f +([^ ]+)/, a) {print a[1]}'`
 
 if [ -f "$FILE" ]; then
-	sed -r -i 's/(.*?)\{\{([^ ]+?)\}\}(.*?)/echo "\1${\2}\3"/ge' $FILE
+	sed -r -i "s/(.*?)\{\{([^ ]+?)\}\}(.*?)/echo -n '\1'\"\${\2}\"'\3'/ge" $FILE
 fi
 echo -n ${KUBE_CA} > /tmp/ca.crt
 kubectl_org -s ${KUBE_SERVER} --token=${KUBE_TOKEN} --certificate-authority=/tmp/ca.crt ${@:1}
