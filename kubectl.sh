bdf40f909afaa7c0eb6b54c17c2df0b02053f7b3
Juan Morales <crispamares@gmail.com>
2017-04-24 11:41:17 +0200

    debug release

--- kubectl.sh
+++ kubectl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "--->" ${@:1}
+
 FILE=`echo -n "${@:1}" | gawk 'match($0, /-f +([^ ]+)/, a) {print a[1]}'`
 
 if [ -f "$FILE" ]; then
 	sed -r -i "s/(.*?)\{\{([^ ]+?)\}\}(.*?)/echo -n '\1'\"\${\2}\"'\3'/ge" $FILE
 fi
-echo -n ${KUBE_CA} > /tmp/ca.crt
-kubectl_org -s ${KUBE_SERVER} --token=${KUBE_TOKEN} --certificate-authority=/tmp/ca.crt ${@:1}
+echo -n ${PLUGIN_CA} > /tmp/ca.crt
+kubectl_org -s ${PLUGIN_SERVER} --token=${PLUGIN_TOKEN} --certificate-authority=/tmp/ca.crt ${@:1}  #TODO !!!! Hacer que pase este parámetro
